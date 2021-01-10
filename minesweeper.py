97d67609e277b0974b4f0306357fc4bba53b237f
Miko <miko.palojarvi@student.oulu.fi>
2021-01-10 23:54:48 +0200

    Added check_square and floodfill

--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,11 @@
 import random
+from math import floor
 import sweeperlib
 
 settings = {
-    "width": 10,
-    "height": 10,
-    "mines": 10,
+    "width": 16,
+    "height": 16,
+    "mines": 80,
     "background_color": (255, 255, 255, 255),
     "window_width": 0,
     "window_height": 0
@@ -17,19 +18,32 @@ mouse_buttons = {
 }
 
 game_status = {
-    "field": [],
+    "current_status": "",
+    "shown_field": [],
+    "hidden_field": [],
     "elapsed_time": 0
 }
 
 def mouse_handler(x, y, button, modifiers):
-    print("Mouse {} clicked in {}, {}".format(mouse_buttons[int(button)], x, y))
+    print("Mouse {} clicked in {}, {}".format(button, x, y))
+    column = floor(x / 40)
+    row = floor(y / 40)
+
+    if button == 1:
+        check_square(row, column)
+    if button == 4:
+        game_status["shown_field"][row][column] = "f"
+        
+
+        
+
 
 def draw_handler():
     
     sweeperlib.clear_window()
     sweeperlib.draw_background()
     sweeperlib.begin_sprite_draw()
-    for i, row in enumerate(game_status["field"]):
+    for i, row in enumerate(game_status["shown_field"]):
         for j, square in enumerate(row):
             sweeperlib.prepare_sprite(square, j*40, i*40)
 
@@ -46,13 +60,13 @@ def interval_handler(elapsed):
     game_status["elapsed_time"] += elapsed
 
 def create_field():
-    field = []
+    game_status["hidden_field"] = []
     for _ in range(settings["height"]):
-        field.append([])
+        game_status["hidden_field"].append([])
         for _ in range(settings["width"]):
-            field[-1].append(" ")
+            game_status["hidden_field"][-1].append(" ")
 
-    game_status["field"] = field
+    game_status["shown_field"] = [row[:] for row in game_status["hidden_field"]]    # This, or deepcopy, or just call create_field again?
 
 def insert_mines():
     free_squares = []
@@ -64,7 +78,38 @@ def insert_mines():
         square = random.choice(free_squares)
         free_squares.remove(square)
         x, y = square
-        game_status["field"][y][x] = "x"
+        game_status["hidden_field"][y][x] = "x"
+        
+def check_square(row, column):
+    square = game_status["hidden_field"][row][column]
+    if square == "x":
+        game_status["shown_field"][row][column] = "x"
+    if square == " ":
+        floodfill(column, row)
+    
+
+def floodfill(starting_x, starting_y):
+    """
+    Marks previously unknown connected areas as safe, starting from the given
+    x, y coordinates.
+    """
+
+    checklist = [(starting_x, starting_y)]
+    i = 0
+    while checklist:
+        x, y = checklist.pop()
+        game_status["shown_field"][y][x] = "0"
+        game_status["hidden_field"][y][x] = "0"
+
+        for i in range(y-1, y+2):
+            if i < 0 or i == settings["height"]:
+                continue
+            for j in range(x-1, x+2):
+                if j < 0 or j == settings["width"]:
+                    continue
+                elif game_status["hidden_field"][i][j] == " ":
+                    checklist.append((j, i))
+        i += 1
 
 def main():
     sweeperlib.load_sprites("sprites")
